807f51b4907dd6aac4b62ac2f83eeee6ead28281
Braxoia <71882015+Braxoia@users.noreply.github.com>
2020-12-21 18:38:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <cstring>
+#include <cassert>
 
 #include "conteneurs.h"
 #include "helpers.h"
@@ -9,11 +10,17 @@
 
 int main(void)
 {
+
     unsigned int nb_pts = 0;
 
+    Plateau plateau;
+    assert(ajouter_plateau(plateau));
+
+    afficher_plateau(plateau);
     Liste_de_liste liste_de_liste;
     initialiser_liste(liste_de_liste);
     lire_liste(liste_de_liste);
+    //dans_le_plateau(p,)
     afficher_liste(liste_de_liste);
     detruire_liste(liste_de_liste);
 
